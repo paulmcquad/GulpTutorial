37acae3cee1537e8300290d0315a27067498a6ce
Paul McQuade <paulmcquad@gmail.com>
2019-06-22 22:12:16 +0100

    Cleaned up Code
    SCSS code
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,11 @@ var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var cleanCSS = require('gulp-clean-css');
 
+
+// Compile SCSS/SASS
 gulp.task('sass', function(){
    return gulp.src('app/style.scss')
       .pipe(sass())
       .pipe(cssnano())
       .pipe(gulp.dest('dist/css'));
 });
-
-
-
